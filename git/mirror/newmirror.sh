d561686a26976ae1d7c221f2d04d8e517a322451
Matt Klich <matt.klich@readytalk.com>
2009-12-14 14:58:24 -0700

    Smarter about where mirror copies get put. Error checking in script.

--- git/mirror/newmirror.sh
+++ git/mirror/newmirror.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
   script=$(basename $0)
   echo "This script will setup a Git mirror of an existing repository."
-  echo "Usage: ${script} <git_url>"
+  echo "Usage: ${script} <mirror_folder> <git_url>"
+  echo "Example: ${script} vlax7 vlax7:/var/local/git/repo.git"
   exit 1
 fi
 
-git clone --mirror $1
+folder=$1
+repo=$2
+
+if [ -d $folder ]
+then
+  (cd $folder && git clone --mirror $repo)
+elif [! -e $folder ]
+  mkdir $folder
+  (cd $folder && git clone --mirror $repo)
+fi
